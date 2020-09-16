0838f981e3fd04700ce802aa895d5e88a1eeccb5
vedenek <svedeneev65@gmail.com>
2020-09-16 15:05:03 +0300

    start progging

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	
+	cout<<"Hello world"<<endl;
+	system("pause");
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
