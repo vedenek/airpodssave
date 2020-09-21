86c12e917e466f63269546bdac94075e9a94ad58
vedenek <71385246+vedenek@users.noreply.github.com>
2020-09-21 12:28:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop 4*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop6 4*/
 using namespace std;
 
 int main(int argc, char** argv) {
